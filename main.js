8dacbaa0b3491c59d65f2fe456003a3771a66ede
Manuel G <manuel.geissberger@gmail.com>
2021-07-20 22:14:31 +0200

    [ADD] small example game

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
 //Example ussage
-class Player{
+class Player {
     constructor() {
         this.x = 10;
         this.y = 10;
-        this.speed = 20;
+        this.w = 8;
+        this.h = 8;
+        this.speed = 40;
+        this.sprite = 0;
+        this.score = 0;
+    }
+}
+
+class Coin {
+    constructor() {
+        this.x = 10;
+        this.y = 10;
+        this.w = 8;
+        this.h = 8;
+        this.sprite = 1;
     }
 }
 
 let player;
+let coin;
+
+function spanCoin() {
+    coin.x = Math.round(Math.random() * (GameEnvironement.graphics.resolutionX-8));
+    coin.y = Math.round(Math.random() * (GameEnvironement.graphics.resolutionY-8));
+}
 
 function init() {
     player = new Player();
+    coin = new Coin();
+    spanCoin();
+}
+
+function collide(obj1, obj2) {
+    if (obj1.y + obj1.h < obj2.y
+        || obj1.y > obj2.y + obj2.h
+        || obj1.x + obj1.w < obj2.x
+        || obj1.x > obj2.x + obj2.w) return false;
+     return true;
 }
 
 function UpdateGame(deltaTime) {
@@ -34,24 +64,29 @@ function UpdateGame(deltaTime) {
         if(down) {
             player.y += player.speed * deltaTime/1000;
         }
-    }
-
-    if(GameEnvironement.input.cmdsUp['jump']) {
-        console.log('jump!');
-    }
+    } 
     
+    //check if player is ontop of coin
+    if(collide(player, coin)) {
+        spanCoin();
+        player.score += 1;
+    }
+}
+
+function drawObject(canvasHandler, object) {
+    canvasHandler.drawSprite(object.sprite, object.x, object.y);
 }
 
 function DrawGame(canvasHandler) {
     canvasHandler.cls();
     
-    canvasHandler.drawSprite(1, 64, 0);
-    canvasHandler.drawSprite(1, 64.5, 8);
-    canvasHandler.drawSprite(8, 64, 16);
-    canvasHandler.drawSprite(0, player.x, player.y);
+    drawObject(canvasHandler, player);
+    drawObject(canvasHandler, coin);
     canvasHandler.drawText("fps: " + Math.round(GameEnvironement.properties.actual_fps), 2, 7, 1, 100)
+    canvasHandler.drawText("score: " + player.score, 135, 7, 1, 100)
 }
 
+GameEnvironement.gameName = 'Saturn91 - coin Collector';
 GameEnvironement.functions.update = UpdateGame;
 GameEnvironement.functions.draw = DrawGame;
 GameEnvironement.functions.init = init;
