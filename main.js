6ef48b8f8083e5c4a05db7383648c149edf0dabd
Manuel G <manuel.geissberger@gmail.com>
2021-07-24 23:58:58 +0200

    [ADD] colllision in utils

--- main.js
+++ main.js
@@ -6,14 +6,6 @@ function init() {
     player = new Player();
 }
 
-function collide(obj1, obj2) {
-    if (obj1.y + obj1.h < obj2.y
-        || obj1.y > obj2.y + obj2.h
-        || obj1.x + obj1.w < obj2.x
-        || obj1.x > obj2.x + obj2.w) return false;
-     return true;
-}
-
 function UpdateGame(deltaTime) {
     player.update(deltaTime);
 }
