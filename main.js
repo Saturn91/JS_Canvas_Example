27745e4eea598846d1443b49adc28949d4b209cf
Manuel G <manuel.geissberger@gmail.com>
2021-07-25 00:20:01 +0200

    [EDIT] clear color updated

--- main.js
+++ main.js
@@ -19,18 +19,21 @@ function DrawGame(canvasHandler) {
 
     player.draw(canvasHandler);
 
-    canvasHandler.drawText("fps: " + Math.round(GameEnvironement.properties.actual_fps), 5, 14, '#ffffff', 100);
+    canvasHandler.drawText("fps: " + Math.round(GameEnvironement.properties.actual_fps), 2, 7, '#ffffff', 100);
 }
 
 GameEnvironement.gameName = 'An Adventure!';
+
 GameEnvironement.functions.update = UpdateGame;
 GameEnvironement.functions.draw = DrawGame;
 GameEnvironement.functions.init = init;
+
 GameEnvironement.graphics.pixelPerfect = false;
 GameEnvironement.graphics.fps = 60;
 GameEnvironement.graphics.autoFitScreen = true;
 GameEnvironement.graphics.resolutionX = 200;
 GameEnvironement.graphics.resolutionY = 150;
+GameEnvironement.graphics.clearColor = '#00E436';
 
 addSpriteSheet('char1_sprites', './assets/char_1_sheet.png', undefined, 16);
 
--- scripts/adventure/characters/player.js
+++ scripts/adventure/characters/player.js
@@ -1,7 +1,7 @@
 class Player {
     constructor() {
-        this.x = GameEnvironement.graphics.resolutionX/2 - 4;
-        this.y = GameEnvironement.graphics.resolutionY/2 - 4;
+        this.x = GameEnvironement.graphics.resolutionX/2 - 8;
+        this.y = GameEnvironement.graphics.resolutionY/2 - 8;
         this.w = 8;
         this.h = 8;
         this.speed = 40;
